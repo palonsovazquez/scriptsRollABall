884b62e50c0699e88377941e978b389b96ef6172
palonsovazquez <pav.vigo@gmail.com>
2019-12-11 10:14:30 +0100

    añadidos comentarios extra·

--- script/twitch_integration_test.cs
+++ script/twitch_integration_test.cs
@@ -14,17 +14,22 @@ public class twitch_integration_test : MonoBehaviour
 
     // panel de texto para el chat
     public Text panelTextoChat;
+    // objetos a los que va a afectar este script
     public GameObject player1;
     public GameObject player2;
     //booleano para activar el debug de esta clase unicamente
-    private bool twitchdebut = true;
+
+    // activa salidas extra en el debug para trazar mejor como entran los mensajes
+    public bool twitchdebug = true;
 
 
 
     // clase de la libreria de Twitch
     public Client cliente;
     void Start()
+
     {
+        panelTextoChat.text = "";
 
         // este metodo permite que el listener siga funcionando aun cuando el juego este en segundo plano
         Application.runInBackground = true;
@@ -74,7 +79,7 @@ public class twitch_integration_test : MonoBehaviour
     // si no es un comando lo agrega al panel de chat del juego.
     private void MyMessageReceivedFunction(object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
     {
-        if (debugprofile.estadodebug || twitchdebut)
+        if (debugprofile.estadodebug || twitchdebug)
         {
 
             Debug.Log("El bot ha recibido un mensaje del chat");
@@ -85,19 +90,21 @@ public class twitch_integration_test : MonoBehaviour
         string textoaux = e.ChatMessage.Message;
         if (e.ChatMessage.IsModerator || e.ChatMessage.Username == "kazuokgi")
         {
-
+            // procesamiento del texto para determinar si es un comando
             if (textoaux[0] == '!')
             {
                 switch (textoaux)
                 {
                     case "!big1":
                         {
+                            // establece el tamaño del jugador 1 al doble
                             player1.gameObject.transform.localScale = new Vector3(2, 2, 2); ;
 
                         }
                         break;
                     case "!big2":
                         {
+                            // establece el tamaño del  jugador 2 al doble
                             player2.gameObject.transform.localScale = new Vector3(2, 2, 2); ;
 
                         }
@@ -105,6 +112,7 @@ public class twitch_integration_test : MonoBehaviour
                         break;
                     case "!little1":
                         {
+                            //establece el tamaño del jugador 1 a la original
                             player1.gameObject.transform.localScale = new Vector3(1, 1, 1); ;
 
 
@@ -112,6 +120,7 @@ public class twitch_integration_test : MonoBehaviour
                         break;
                     case "!little2":
                         {
+                            // establece el tamaño del jugador 2 al original
                             player2.gameObject.transform.localScale = new Vector3(1, 1, 1); ;
 
 
@@ -125,7 +134,10 @@ public class twitch_integration_test : MonoBehaviour
             }
             else
             {
-                panelTextoChat.text += "\n" + e.ChatMessage.Username + " : " + e.ChatMessage.Message;
+                // si no es ningun comando muestra el mensaje en el juego como un chatlog
+                panelTextoChat.text
+                += "\n" + e.ChatMessage.Username
+                + " : " + e.ChatMessage.Message;
 
 
 
